424b2db18d2eb70b2c47233feeed010d2efd2876
ThanhVu Nguyen <tnguyen@cs.unm.edu>
2012-05-17 18:28:16 -0600

    some more reorganization and examples

--- Generalizations/1117c_STABLE_PATCH_ONLY/JOLT.h
+++
@@ -1,8 +0,0 @@
-/*shuffling*/
-void jolt(int tempConfigA[],const int &JOLT_PERCENT, const int &alphaMaxK){
-  for(int i =0; i < nVertices ; ++i){
-	if(genrand_int32()%100 <= JOLT_PERCENT){
-	  tempConfigA[i]=genrand_int32()%alphaMaxK;
-	}
-  }
-}
--- Generalizations/generalization-readme.txt
+++ Generalizations/generalization-readme.txt
@@ -1,3 +1,6 @@
+5/17/2012
+mv 1117c_STABLE_PATCH_ONLY/ to Code/
+
 tscript.sh => /storage/Users/thanhvu/Bandwidth/Src/tvn/tscript.sh
 
 ========
